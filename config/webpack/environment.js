884645d0b27823f1488ead52edbbe89e3580264f
42-hoshino-M <119111980+dexias9024@users.noreply.github.com>
2023-10-11 15:16:03 +0900

    Revert "Deploy"

--- config/webpack/environment.js
+++
@@ -1,6 +0,0 @@
-const { environment } = require('@rails/webpacker')
-environment.entryPoints
-  .set('application', './app/javascript/packs/application')
-  .set('styles', './app/javascript/stylesheets/application')
-
-module.exports = environment
